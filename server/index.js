6e4a9f0081f78ad0b17d612dcb5c16766652327c
Nathan Young <nathanyoung@Nathans-MacBook-Pro.local>
2015-12-18 13:36:13 -0800

    add index.js

---
+++ server/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var _ = require('lodash');
+
+// create application
+var app = express();
+
+// add middleware necessary for REST API's
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
